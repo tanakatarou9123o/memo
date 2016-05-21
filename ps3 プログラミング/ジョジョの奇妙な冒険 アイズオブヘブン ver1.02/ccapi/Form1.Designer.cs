c34d750c83cc8d62408b1818243f113772c2402a
tarou tanaka <tarou tanaka>
2016-05-22 01:10:10 +0900

    コミットメッセージ

---
+++ ps3 プログラミング/ジョジョの奇妙な冒険 アイズオブヘブン ver1.02/ccapi/Form1.Designer.cs
@@ -0,0 +1,221 @@
+﻿namespace ccapi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(39, 46);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 19);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "192.168.11.64";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(39, 72);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(120, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "JP減らない";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(39, 102);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(120, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "デュアルヒートMAX";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(39, 132);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(120, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "デュアルコンボMAX";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(165, 77);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(27, 12);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "OFF";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(165, 107);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(27, 12);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "OFF";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(165, 137);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(27, 12);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "OFF";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(213, 46);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "PS3と接続";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Location = new System.Drawing.Point(332, -1);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(832, 632);
+            this.webBrowser1.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(165, 165);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(27, 12);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "OFF";
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(39, 160);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(120, 23);
+            this.button5.TabIndex = 10;
+            this.button5.Text = "アビリティポイントMAX";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(39, 189);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(80, 23);
+            this.button6.TabIndex = 11;
+            this.button6.Text = "経験値n倍";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(165, 189);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(27, 12);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "OFF";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(125, 191);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(34, 19);
+            this.textBox2.TabIndex = 13;
+            this.textBox2.Text = "4";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(835, 617);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.webBrowser1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "ジョジョの奇妙な冒険 アイズオブヘブン ver1.02";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ ps3 プログラミング/ジョジョの奇妙な冒険 アイズオブヘブン ver1.02/ccapi/Form1.cs
@@ -0,0 +1,215 @@
+﻿using PS3Lib;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ccapi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private CCAPI PS3 = new CCAPI();
+        uint adores = 0;
+        string ip;
+
+        bool flag1 = false;
+        bool flag2 = false;
+        bool flag3 = false;
+        bool flag4 = false;
+        bool flag5 = false;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            button1.Enabled = false;
+            button2.Enabled = false;
+            button3.Enabled = false;
+            button5.Enabled = false;
+            button6.Enabled = false;
+
+            string[] files = System.IO.Directory.GetFiles(Application.StartupPath,"*.txt");
+            Regex regex = new Regex(@"\\[0-9.][0-9.][0-9.][0-9.].*?.txt$");
+            MatchCollection matchCol = regex.Matches(files[0]);
+            foreach (Match match in matchCol)
+            {
+                Match xxx = match;
+                string a=xxx.ToString().Replace(".txt", "");
+                a = a.ToString().Replace("\\", "");
+                textBox1.Text = a;
+            }
+
+            webBrowser1.ScriptErrorsSuppressed = true;
+            webBrowser1.Navigate("http://games24.blog.fc2.com/");
+
+        }
+
+        byte[] stringTobytes(string bytest)
+        {
+            int length = bytest.Length / 2;//文字数からバイト数取得
+            byte[] bytes = new byte[length];//バイト数分の配列
+            int j = 0;
+            for (int i = 0; i < length; i++)
+            {
+                bytes[i] = Convert.ToByte(bytest.Substring(j, 2), 16);
+                j += 2;
+            }
+
+            return bytes;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            adores = 0x000893D4;
+
+            if (!flag1)
+            {
+                string bytest = "91230000";
+                byte[] s = stringTobytes(bytest);
+
+                PS3.Extension.WriteBytes(adores, s);
+                adores = 0x0089368;
+                byte[] sss = stringTobytes("91690000");
+                PS3.Extension.WriteBytes(adores, sss);
+
+                flag1 = true;
+                label1.Text = "ON";
+                return;
+            }
+            
+            string bytest1 = "90030000";
+            byte[] ss = stringTobytes(bytest1);
+
+            PS3.Extension.WriteBytes(adores, ss);
+            adores = 0x0089368;
+            byte[] ssss = stringTobytes("90090000");
+            PS3.Extension.WriteBytes(adores, ssss);
+
+            flag1 = false;
+            label1.Text = "OFF";
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            adores = 0x007EC710;
+            if (!flag2)
+            {
+                string bytest = "60000000";
+                byte[] s = stringTobytes(bytest);
+
+                PS3.Extension.WriteBytes(adores, s);
+                
+                flag2 = true;
+                label2.Text = "ON";
+                return;
+            }
+            string bytestt = "409D000C";
+            byte[] ss = stringTobytes(bytestt);
+
+            PS3.Extension.WriteBytes(adores, ss);
+            flag2 = false;
+            label2.Text = "OFF";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            adores = 0x007EC77C;
+            if (!flag3)
+            {
+                string bytest = "60000000";
+                byte[] s = stringTobytes(bytest);
+
+                PS3.Extension.WriteBytes(adores, s);
+
+                flag3 = true;
+                label3.Text = "ON";
+                return;
+
+            }
+            string bytest1 = "409D000C";
+            byte[] ss = stringTobytes(bytest1);
+
+            PS3.Extension.WriteBytes(adores, ss);
+            flag3 = false;
+            label3.Text = "OFF";
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            int fl = PS3.ConnectTarget(textBox1.Text);
+            if (!PS3.SUCCESS(fl))
+            {
+                MessageBox.Show("PS3.ConnectTarget失敗");
+                return;
+            }
+
+            PS3.AttachProcess();
+
+            button1.Enabled = true;
+            button2.Enabled = true;
+            button3.Enabled = true;
+            button5.Enabled = true;
+            button6.Enabled = true;
+        }
+        
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            adores = 0x0006006C;
+            if (!flag4)
+            {
+                string bytest = "60000000";
+                byte[] s = stringTobytes(bytest);
+
+                PS3.Extension.WriteBytes(adores, s);
+
+                flag4 = true;
+                label4.Text = "ON";
+                return;
+
+            }
+            string bytest1 = "409D0008";
+            byte[] ss = stringTobytes(bytest1);
+
+            PS3.Extension.WriteBytes(adores, ss);
+            flag4 = false;
+            label4.Text = "OFF";
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            adores = 0x0005EB64;
+            if (!flag5)
+            {
+                int jj=int.Parse(textBox2.Text);
+                string bytest = "1D7C00"+jj.ToString("X2");
+                byte[] s = stringTobytes(bytest);
+
+                PS3.Extension.WriteBytes(adores, s);
+
+                flag5 = true;
+                label5.Text = "ON";
+                return;
+
+            }
+            string bytest1 = "7F8BE378";
+            byte[] ss = stringTobytes(bytest1);
+
+            PS3.Extension.WriteBytes(adores, ss);
+            flag5 = false;
+            label5.Text = "OFF";
+        }
+    }
+}
