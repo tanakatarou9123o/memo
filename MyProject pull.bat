c1e3507f067efec7ea15dfe5d8af312877bfe24b
tarou tanaka <tarou tanaka>
2016-05-23 23:15:23 +0900

    batt

--- MyProject pull.bat
+++ MyProject pull.bat
@@ -1,3 +1,5 @@
 @echo off
 cd C:\Users\tanak\Documents\Unreal Projects\MyProject
 git pull origin master
+cd /d D:\1
+git pull origin master
--- MyProject push.bat
+++ MyProject push.bat
@@ -3,3 +3,7 @@ cd C:\Users\tanak\Documents\Unreal Projects\MyProject
 git add --all
 git commit -a -m batt
 git push origin master
+cd /d D:\1
+git add --all
+git commit -a -m batt
+git push origin master
